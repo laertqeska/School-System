b355f08b9576f5288c95eb9bfcd707ab2c1aa27d
laertqeska <laertqeska@gmail.com>
2025-07-22 18:11:56 +0200

    initial commit

---
+++ src/main/java/com/example/School_System/SchoolSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.School_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSystemApplication {
+//test
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolSystemApplication.class, args);
+	}
+
+}
