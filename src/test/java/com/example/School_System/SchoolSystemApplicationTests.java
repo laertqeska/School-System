8ca350ff658b844d5281065f90b22c9260c6b437
laertqeska <laertqeska@gmail.com>
2025-07-22 18:11:56 +0200

    initial commit

---
+++ src/test/java/com/example/School_System/SchoolSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.School_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
